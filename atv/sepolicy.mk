LineageOS/android_device_lineage_sepolicy

atv: Label and allow mediashell_app what it needs
* ATV GMS does this, so we're gonna have to as well.

Change-Id: I0d4fecfad032b0a14a215fa4ddf2e994a9df0c70

atv/sepolicy.mk
@@ -7,3 +7,6 @@ ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/lineage/sepolicy/atv/vendor
 endif
+
+PRODUCT_PRIVATE_SEPOLICY_DIRS += \
+   device/lineage/sepolicy/atv/private

DerpFest-12/device_derp_sepolicy

atv/sepolicy.mk
@@ -7,3 +7,6 @@ ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
     device/derp/sepolicy/atv/vendor
 endif
+
+PRODUCT_PRIVATE_SEPOLICY_DIRS += \
+   device/derp/sepolicy/atv/private